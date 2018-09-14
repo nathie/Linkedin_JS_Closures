21f35e03b89084b8fdeac2ca07b7388986a76058
Gleidy Mendoza <gleidymendoza@gmail.com>
2018-09-14 15:57:30 -0700

    Solution with forEach

--- Exercise Files/Ch01/01_01/start/js/script.js
+++ Exercise Files/Ch01/01_01/start/js/script.js
@@ -67,13 +67,20 @@ const listWidget = (function() {
       document.querySelector('.results').classList.add('open');
     },
     updateUIWorking: function() {
-      for(var i = 0; i <= 9; i++) {
-        (function(j){
-          setTimeout(function(){
-            document.querySelector('.conditions').innerHTML = `<p class="animation">00:00.${j}</p>`;
-          }, 100 * j);
-        })(i);
-      }
+      var count = [0,1,2,3,4,5,6,7,8,9];
+      count.forEach(function(el){
+        setTimeout(function(){
+          document.querySelector('.conditions').innerHTML = 
+            `<p class="animation">00:00.${el}</p>`;
+        }, 100 * el);
+      });
+      // for(var i = 0; i <= 9; i++) {
+      //   (function(j){
+      //     setTimeout(function(){
+      //       document.querySelector('.conditions').innerHTML = `<p class="animation">00:00.${j}</p>`;
+      //     }, 100 * j);
+      //   })(i);
+      // }
     },
 
     updateUISuccess: function(response) {
